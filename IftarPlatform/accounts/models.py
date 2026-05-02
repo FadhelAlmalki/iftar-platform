b919421a58e2faac394b15cd3a481969dc0b4c36
Ali Alshehri <ps4ps5cc@gmail>
2026-05-02 17:21:23 +0300

    added to models,urls and views of accounts app

--- IftarPlatform/accounts/models.py
+++ IftarPlatform/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+class Profile(models.Model):
+    
+    class Role(models.TextChoices):
+        OWNER = 'owner', 'Owner'
+        ORGANIZER = 'organizer', 'Organizer'
+        ADMIN = 'admin', 'Admin'
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    role = models.CharField(max_length=20, choices=Role.choices)
+    rep_id = models.CharField(max_length=20, blank=True)
+    entity_name = models.CharField(max_length=200, blank=True)
+    about = models.TextField(blank=True)
+    image = models.ImageField(upload_to='profiles/', blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
--- IftarPlatform/accounts/urls.py
+++ IftarPlatform/accounts/urls.py
@@ -1,2 +1,13 @@
 from django.urls import path
 from . import views
+
+app_name="accounts"
+
+urlpatterns=[
+
+    path('role-select/<str:action>/', views.role_select_view, name='role_select_view'),
+    
+    path('signup/<str:role>/', views.signup_view, name='signup_view'),
+    path('signin/<str:role>/', views.signin_view, name='signin_view'),
+    path('logout/', views.logout_view, name='logout_view'),
+]
