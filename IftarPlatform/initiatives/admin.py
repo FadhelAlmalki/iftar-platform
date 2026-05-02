4730bc1001ce650293a5d338ef355166de350d76
Fadhel Almalki <fm4680@gmail.com>
2026-05-02 17:32:08 +0300

    Added admin and paths and main app name in urls.py

--- IftarPlatform/initiatives/admin.py
+++ IftarPlatform/initiatives/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Initiative, City
+
+class InitiativeAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'title', 'description', 'city', 'place', 'image', 'created_at', 'starts_at', 'ends_at', 'init_status', 'is_available')
+    list_filter = ('init_status', 'city', 'is_available')
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+
+
+admin.site.register(Initiative, InitiativeAdmin)
+admin.site.register(City, CityAdmin)
