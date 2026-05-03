c34f971324c6ab3a382cc5aade61ff36d421b0a0
Ali Alshehri <ps4ps5cc@gmail>
2026-05-03 20:10:08 +0300

    uniform admin.py in accounts app with other apps in writing way

--- IftarPlatform/accounts/admin.py
+++ IftarPlatform/accounts/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Profile
 # Register your models here.
 
-@admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'role', 'entity_name', 'rep_id']
     list_filter = ['role']
     search_fields = ['user__username', 'entity_name']
+
+admin.site.register(Profile, ProfileAdmin)
