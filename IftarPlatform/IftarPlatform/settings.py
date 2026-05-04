bab2be447dfa0a3bad1d74275c3d7dc731bc5aaa
FadhelAlmalki <almalkifadhel@gmail.com>
2026-05-04 23:45:11 +0300

    try to fix

--- IftarPlatform/IftarPlatform/settings.py
+++ IftarPlatform/IftarPlatform/settings.py
@@ -135,7 +135,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = []
+project_static_dir = os.path.join(BASE_DIR, "static")
+if os.path.isdir(project_static_dir):
+    STATICFILES_DIRS.append(project_static_dir)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
